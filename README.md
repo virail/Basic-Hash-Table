71a9f798daedc3c633fa8dc3ba7e5cac4b07c72f
virail <xjbviralx@gmail.com>
2024-05-09 23:01:36 +0100

    initial commit, finished tutorial, added readme for info

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linear probing hash table
+
+Following the tutorial for creating a hash table via [build-your-own-x](https://github.com/codecrafters-io/build-your-own-x)
---
+++ hash_table.h
@@ -0,0 +1,23 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+typedef struct {
+  char* key;
+  char* value;
+} ht_item;
+
+typedef struct {
+  int base_size;
+  int size;
+  int count;
+  ht_item** items;
+} ht_hash_table;
+
+ht_hash_table* ht_new();
+void ht_del_hash_table(ht_hash_table* ht);
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
+
+#endif
