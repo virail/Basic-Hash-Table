7b712bcb9a67f6edb7d061e3b87f35cd1a452d61
virail <xjbviralx@gmail.com>
2024-05-10 21:50:41 +0100

    added basic timing to see performance, currently not precise enough, and not testing enough operations

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+
 #include "hash_table.h"
 
 int main()
 {
+  
+  clock_t start = clock();
 
   ht_hash_table* ht = ht_new();
+  ht_insert(ht, "first", "yipee");
+  ht_insert(ht, "first", "second");
+  ht_insert(ht, "league", "yipee");
+
+  char* searched_val = ht_search(ht, "first");
+
   ht_del_hash_table(ht);
+
+  clock_t end = clock();
+
+  printf("searched key: first\nfound value: %s\n", searched_val);
+
+  double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+  printf("time taken: %f seconds\n", cpu_time_used);
+
+  return 0;
 
 }
